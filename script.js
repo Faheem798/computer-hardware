7340d0316fff00705344b3a6684fca02462c1b57
faheemrana798 <faheemrana798@gmail.com>
2023-10-02 11:16:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const videoCon = document.querySelector('#videocontainer')
+const play = document.querySelector('#play')
+
+function videoConanim(){
+    videoCon.addEventListener('mouseenter',()=>{
+        gsap.to(play,{
+            scale:1,
+            opacity:0.6
+        })
+    })
+    videoCon.addEventListener('mouseout',()=>{
+        gsap.to(play,{
+            scale:0,
+            opacity:0
+        })
+    })
+    videoCon.addEventListener('mousemove',(dets)=>{
+        gsap.to(play,{
+            top: dets.y- 80,
+            left:dets.x-80
+    
+        })
+    
+    })
+}
+videoConanim()
+gsap.from('.main h1',{
+    y: 29,
+    opacity: 0,
+    delay: 0.29,
+    duration:0.83,
+    stagger: 0.13
+})
+gsap.from('#videocontainer',{
+    y: 29,
+    opacity: 0,
+    scale: 0.33,
+    delay: 0.39,
+    duration:0.33,
+})
